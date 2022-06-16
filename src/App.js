327ee9cedc983696811d602f8612f71630328d7e
lulu <798809718@qq.com>
2022-06-16 17:19:35 +0800

    引入 antd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import { Button,message } from 'antd'
+import 'antd/dist/antd.less'
 
 export default class App extends Component {
+  handleClick = () => {
+    message.success('成功啦...');
+  } 
   render () {
     return (
-      <div>App3</div>
+      <div>
+        <Button type="primary" onClick={this.handleClick}>测试按钮</Button>
+      </div>
     )
   }
 }
