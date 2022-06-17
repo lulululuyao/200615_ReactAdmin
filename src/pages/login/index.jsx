6601fe185f7808249e3bad7de447a67ff183940c
lulu <798809718@qq.com>
2022-06-17 09:20:03 +0800

    202206170919-当前版本更新

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,6 +5,10 @@ export default class Login extends Component {
   render() {
     return (
       <div className='login'>
+        <header className='login-header'>
+          <img src='' alt='logo'></img>
+          <h1>React项目：后台管理系统</h1>
+        </header>
         <header className='login-header'></header>
         <section className='login-content'></section>
       </div>
