b89140e91c4cee3fd861e8c426dc090b45fecb4d
lulu <798809718@qq.com>
2022-06-18 11:49:17 +0800

    2022-06-18 11:49 当前更新

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,16 +1,44 @@
 import React, { Component } from 'react'
 import './login.less'
+import logo from './images/logo.png'
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd'
 
 export default class Login extends Component {
-  render() {
+  onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  render () {
     return (
       <div className='login'>
         <header className='login-header'>
-          <img src='' alt='logo'></img>
+          <img src={logo} alt='logo' />
           <h1>React项目：后台管理系统</h1>
         </header>
-        <header className='login-header'></header>
-        <section className='login-content'></section>
+        <section className='login-content'>
+          <h2>用户登录</h2>
+          <div>
+            <Form className='login-form' name='basic' initialValues={{ remember: true }}
+              onFinish={this.onFinish} onFinishFailed={this.onFinishFailed} autoComplete='off'>
+              <Form.Item name='username' rules={[{required: true,message: 'Please input your username!'}]}>
+                <Input prefix={<UserOutlined className="site-form-item-icon" style={{color:'rgba(0,0,0,.25'}}/>} placeholder="Username" />
+              </Form.Item>
+              <Form.Item name='password' rules={[{required: true,message: 'Please input your password!'}]}>
+                <Input prefix={<LockOutlined className="site-form-item-icon" style={{color:'rgba(0,0,0,.25'}}/>} type="password" placeholder="Password"/>
+              </Form.Item>
+              <Form.Item name='remember' valuePropName='checked' >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+              <Form.Item >
+                <Button className='login-form-btn' type='primary' htmlType='submit'>提交</Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </section>
       </div>
     )
   }
